e27c66793cc9e6ad0b987b528f0f28a363a31735
kubica4u2 <kubica4u2@gmail.com>
2024-01-05 18:54:43 -0600

    update telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
         tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
-            {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
+            {'<leader>lg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
             {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
             {'<leader>gf', "<cmd>Telescope git_files<cr>", desc = "Search [G]it [F]iles"},
             {'<leader>sh', "<cmd>Telescope help_tags<cr>", desc = "[S]earch [H]elp"},
