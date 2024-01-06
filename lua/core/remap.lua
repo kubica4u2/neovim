cbcf403ef4c540a4232ac5e20a3a31301f038b99
kubica4u2 <kubica4u2@gmail.com>
2024-01-05 18:28:47 -0600

    init push

---
+++ lua/core/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>h", vim.cmd.Ex)
+
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        keys = {
+            {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
+            {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
+            {'<leader>gf', "<cmd>Telescope git_files<cr>", desc = "Search [G]it [F]iles"},
+            {'<leader>sh', "<cmd>Telescope help_tags<cr>", desc = "[S]earch [H]elp"},
+            {'<leader>sw', "<cmd>Telescope grep_string<cr>", desc = "[S]earch current [W]ord"}
+        }
+}
