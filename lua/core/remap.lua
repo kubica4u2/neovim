36a5ff84352656fa4407f65b738156899278840f
kubica4u2 <kubica4u2@gmail.com>
2024-01-13 11:27:14 -0600

    Dev - Add toggleter, fugitive, and gitgutter

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>hh", vim.cmd.Ex)
 
+
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,14 +4,5 @@ return {
     opts = {
         size = 20,
         open_mapping = [[<c-\>]],
-        direction = 'float',
-        float_opts = {
-            border = 'curved',
-            winblend = 3,
-            highlights = {
-                border = "Normal",
-                background = "Normal",
-            },
-        }
     }
 }
